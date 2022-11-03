cf5a37b7ca33ee4e393b08f1e404a8bab6d0abc0
Dmitriy Shaidurov <dmitriyshaidurov@yandex.ru>
2022-11-03 17:43:24 +0500

    createUser400

--- controllers/users.js
+++ controllers/users.js
@@ -33,14 +33,14 @@ const createUser = (req, res) => {
     })
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
-        return res.status(404).send({ message: 'Ошибка валидации', err });
+        return res.status(400).send({ message: 'Ошибка валидации', err });
       }
       return res.status(500).send({ message: 'На сервере произошла ошибка', err });
     });
 };
 const updateUser = (req, res) => {
   User.findByIdAndUpdate(req.user._id, { name: req.body.name, about: req.body.about }).orFail(new Error('NotFound'))
-    .then((user) => res.status(201).send(user))
+    .then((user) => res.status(200).send(user))
     .catch((err) => {
       if (err.message === 'NotFound') {
         return res.status(404).send({ message: 'Пользователь с указанным _id не найден.' });
