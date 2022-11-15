15a8cfad327161bd04fa719b30e7cd26a5cd715f
Dmitriy Shaidurov <dmitriyshaidurov@yandex.ru>
2022-11-15 16:55:10 +0500

    1ошибка

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUsers, getUserById, updateUser, updateUserAvatar, getUser,
@@ -8,7 +9,11 @@ router.get('/users/me', getUser);
 
 router.get('/users', getUsers);
 
-router.get('/users/:userId', getUserById);
+router.get('/users/:userId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().pattern(/[a-f0-9]{24,24}/).length(24),
+  }),
+}), getUserById);
 
 router.patch('/users/me', updateUser);
 
