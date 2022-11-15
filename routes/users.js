1e82231b7447f819af7bda256b47b70b43d78b4b
Dmitriy Shaidurov <dmitriyshaidurov@yandex.ru>
2022-11-15 17:24:15 +0500

    ghdfhjdfjf

--- routes/users.js
+++ routes/users.js
@@ -9,14 +9,14 @@ router.get('/users/me', getUser);
 
 router.get('/users', getUsers);
 
+router.patch('/users/me', updateUser);
+
+router.patch('/users/me/avatar', updateUserAvatar);
+
 router.get('/users/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string.id().length(24),
   }),
 }), getUserById);
-
-router.patch('/users/me', updateUser);
-
-router.patch('/users/me/avatar', updateUserAvatar);
 
 module.exports = router;
