db56cf4d11af685c7aa522b57a84e8ee26731a92
Dmitriy Shaidurov <dmitriyshaidurov@yandex.ru>
2022-11-15 16:04:25 +0500

    22

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,19 +3,19 @@ const AuthError = require('../errors/authErr');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-  if (!token) {
-    throw new AuthError('Необходима авторизация');
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new AuthError('Необоходима авторизация');
   }
 
+  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, 'secret-key');
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    throw new AuthError('Необходима авторизация');
+    throw new AuthError('Необоходима авторизация');
   }
-
   req.user = payload;
-
   next();
 };
