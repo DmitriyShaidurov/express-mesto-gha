3e9066a2c09a67a99830a4439dd0ce66b509ca26
Dmitriy Shaidurov <dmitriyshaidurov@yandex.ru>
2022-11-15 17:59:15 +0500

    name,link

--- routes/cards.js
+++ routes/cards.js
@@ -7,12 +7,14 @@ const {
 
 router.get('/cards', getCard);
 
-router.post('/cards', createCard);/* celebrate({
+router.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().pattern(),
+    link: Joi.string().required().pattern(
+      /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*$/,
+    ),
   }),
-}),  */
+}), createCard);
 
 router.delete('/cards/:cardId', celebrate({
   params: Joi.object().keys({
